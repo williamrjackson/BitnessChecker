f6ebbe38c8374d60b1e89b0b617053d58167b3a2
Bill Jackson <billjackson@cakewalk.com>
2018-03-08 04:29:33 -0500

    Removed support for multiple args
    Opting for error codes over support for multiple files. Allows for
    using this utility in other scripts more easily.

--- bitness.bat
+++ bitness.bat
@@ -3,26 +3,16 @@ setlocal enabledelayedexpansion
 
 if [%1]==[] goto usage
 
-set argCount=0
-for %%x in (%*) do (
-   set /A argCount+=1
-   set "argVec[!argCount!]=%%~x"
-)
-
-for /L %%i in (1,1,%argCount%) do (
-	echo.
-	echo !argVec[%%i]!:
-	%~dp0\dumpbin.exe /headers "!argVec[%%i]!" | findstr /I /C:"machine (x64)" > NUL && (
-		echo x64
-		exit /B 0
+%~dp0\dumpbin.exe /headers "%1" | findstr /I /C:"machine (x64)" > NUL && (
+	echo x64
+	exit /B 0
+) || (
+	%~dp0\dumpbin.exe /headers "%1" | findstr /I /C:"machine (x86)" > NUL && (
+		echo x86
+		exit /B 1
 	) || (
-		%~dp0\dumpbin.exe /headers "!argVec[%%i]!" | findstr /I /C:"machine (x86)" > NUL && (
-			echo x86
-			exit /B 1
-		) || (
-			echo Undetermined
-			exit /B 2
-		)
+		echo Undetermined
+		exit /B 2
 	)
 )
 
